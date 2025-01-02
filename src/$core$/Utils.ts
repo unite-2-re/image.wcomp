c722a04a9536a6b5d04a5774b8cad170ac3d05ba
u2re-dev <conway24b@yandex.ru>
2025-01-02 10:33:11 +0700

    reduce code

---
+++ src/$core$/Utils.ts
@@ -0,0 +1,61 @@
+//
+export const getCorrectOrientation = () => {
+    let orientationType: string = screen.orientation.type;
+    if (!window.matchMedia("((display-mode: fullscreen) or (display-mode: standalone) or (display-mode: window-controls-overlay))").matches) {
+        if (matchMedia("(orientation: portrait)").matches) {orientationType = orientationType.replace("landscape", "portrait");} else
+            if (matchMedia("(orientation: landscape)").matches) {orientationType = orientationType.replace("portrait", "landscape");};
+    }
+    return orientationType;
+};
+
+//
+/*export const orientationNumberMap = {
+    "portrait-primary": 0, // as 0deg, aka. 360deg
+    "landscape-primary": 1, // as -90deg, aka. 270deg
+    "portrait-secondary": 2, // as -180deg, aka. 180deg
+    "landscape-secondary": 3 // as -270deg, aka. 90deg
+}*/
+
+//
+export const orientationNumberMap = {
+    "landscape-primary": 0, // as 0deg, aka. 360deg
+    "portrait-primary": 1, // as -90deg, aka. 270deg
+    "landscape-secondary": 2, // as -180deg, aka. 180deg
+    "portrait-secondary": 3, // as -270deg, aka. 90deg
+}
+
+//
+const delayed = new Map<number, Function | null>([]);
+requestIdleCallback(async ()=>{
+    while(true) {
+        for (const dl of delayed.entries()) {
+            dl[1]?.(); delayed.delete(dl[0]);
+        }
+
+        //
+        try { await (new Promise((rs)=>requestAnimationFrame(rs))); } catch(e) { break; };
+    }
+}, {timeout: 1000});
+
+//
+export const callByFrame = (pointerId, cb)=>{
+    delayed.set(pointerId, cb);
+}
+
+//
+export const cover = (ctx, img, scale = 1, port, orient = 0) => {
+    const canvas = ctx.canvas;
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.rotate((orient || 0) * (Math.PI * 0.5));
+    ctx.rotate(port * -(Math.PI / 2));
+    ctx.translate(-(img.width / 2) * scale, -(img.height / 2) * scale);
+};
+
+//
+const blobImageMap = new WeakMap();
+export const createImageBitmapCache = (blob)=>{
+    if (!blobImageMap.has(blob) && (blob instanceof Blob || blob instanceof File || blob instanceof OffscreenCanvas || blob instanceof ImageBitmap || blob instanceof Image)) {
+        blobImageMap.set(blob, createImageBitmap(blob));
+    }
+    return blobImageMap.get(blob);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import UCanvas from "./$core$/Canvas";
 export * from "./$core$/Canvas";
 export default UCanvas;
+
+// @ts-ignore
+import { loadBlobStyle } from "/externals/lib/dom.js";
+
+// @ts-ignore
+import styles from "./$scss$/Canvas.scss?inline&compress";
+loadBlobStyle(styles);
